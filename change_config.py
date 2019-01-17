06709c1cb6d08c64d4cf54800846ec7ea9027beb
drchilly <dragan@netvirtexpert.com>
2019-01-17 15:14:02 +0100

    Update change_config.py

--- change_config.py
+++ change_config.py
@@ -3,18 +3,21 @@ import os
 import sys
 
 def change_config():
-    driver = napalm.get_network_driver('ios')
+driver = napalm.get_network_driver('ios')
 
 
-    with open (r'router_list.txt', 'r') as ip_input:
-        for ip in ip_input:
-            ip = ip.strip()
+    #with open (r'router_list.txt', 'r') as ip_input:
+     #   for ip in ip_input:
+      #      ip = ip.strip()
 
     # Connect: PUT ADEQUATE USERNAME AND password
-            device = driver(hostname=ip, username='dragan.ilic', password='wersdf123!')
-
+device = driver(hostname='x.x.x.x', username='???', password='???')
+#print ('Openning...')
     #MAKE CONFIG CHANGE
-            device.open()
-            device.load_merge_candidate(config='interface Loopback201\ndescription TEST\nip address 111.111.111.111 255.255.255.0')
-
-            print (device.compare_config())
+device.open()
+device.load_merge_candidate(config='interface Loopback201\ndescription TEST\nip address 111.111.111.111 255.255.255.0')
+            
+print (device.compare_config())
+device.close()
+if __name__ == '__change_config__':
+    change_config()
